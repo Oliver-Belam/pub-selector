9c599f880b4edac1e784c59abbc44d163ef52f46
oliver-belam <oliver.t.belam@gmail.com>
2025-08-16 14:25:40 +0100

    Split.py created, should handle the raw data safely

--- split.py
+++ split.py
@@ -10,13 +10,34 @@ and splits it into two normalized csv files:
 import csv
 from pathlib import Path
 
+raw_csv = Path("data/york_pubs.csv")
+pubs_csv = Path("data/pubs.csv")
+tags_csv = Path("data/tags.csv")
 
+# This will rewrite both pubs_csv and tag_csv each time split.py is run
+with open(raw_csv, newline="", encoding="utf-8") as rawfile, \
+     open(pubs_csv, "w", newline="", encoding="utf-8") as pubfile, \
+     open(tags_csv, "w", newline="", encoding="utf-8") as tagfile:
 
-with open("data/york_pubs.csv", newline="", encoding="utf-8") as rawfile, \
-     open("data/pubs.csv", "w", newline="", encoding="utf-8") as pubfile, \
-     open("data/tags.csv", "w", newline="", encoding="utf-8") as tagfile:
-
+    # Reads the rawfile into a dictionary
     reader = csv.DictReader(infile)
     pub_writer = csv.writer(pubfile)
     tag_writer = csv.writer(tagfile)
 
+    # Write the headers
+    pub_writer.writerow(["pub_id", "pub_name"])
+    tag_writer.writerow(["pub_id", "tag"])
+
+    # Enumerate gives each pub a unique ID starting at 1
+    for pub_id, row in enumerate(reader, start=1):
+        pub_name = row["name"]
+        pub_writer.writerow([pub_id, pub_name])
+
+        # Split the tags column by commas
+        tags = row["tags"].split(",")
+
+        # Goes through each tag and removes spaces and writes to tags_csv
+        for tag in tags:
+            clean_tag = tag.strip()
+            if clean_tag:
+                tag_writer.writerow([pub_id,clean_tag])
